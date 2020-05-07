fa364237e5f0a811986dfb02b31171e1a0e652e2
oskar <oskar.vedel@gmail.com>
2020-05-07 11:28:11 +0200

    .

--- RCCS_Auth_Test_Project/ClientApp/src/App.js
+++ RCCS_Auth_Test_Project/ClientApp/src/App.js
@@ -17,6 +17,7 @@ export default class App extends Component {
         <Route exact path='/' component={Home} />
         <Route path='/counter' component={Counter} />
         <Route path='/fetch-data' component={FetchData} />
+          <Route path='/log-in' component={LogIn} />
       </Layout>
     );
   }
--- RCCS_Auth_Test_Project/ClientApp/src/components/LogIn.js
+++ RCCS_Auth_Test_Project/ClientApp/src/components/LogIn.js
@@ -1,87 +1,54 @@
-/*
 import React, {Component} from 'react';
-import { Formik, Field, Form, ErrorMessage } from 'formik';
-import * as Yup from 'yup';
 import axios from 'axios';
 
-class LoginPage extends React.Component {
+export class LogIn extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {value: ''};
 
-        const apiUrl = 'https://your-api.com/';
-        
-        //TO DO
-        // redirect to home if already logged in
-        
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
     }
-    render() {
-        return (
-            <div>
-            <div className="alert alert-info">
-            Username: test<br />
-        Password: test
-        </div>
-        <h2>Login</h2>
-        <Formik
-        initialValues={{
-            username: '',
-                password: ''
-        }}
-        validationSchema={Yup.object().shape({
-                username: Yup.string().required('Username is required'),
-                password: Yup.string().required('Password is required')
-            })}
-        onSubmit={({ username, password }, { setStatus, setSubmitting }) => {
-            setStatus();
-            const response = await fetch('authenticate');
-            const data = await response.json();
-            this.setState({ forecasts: data, loading: false });
-            authenticationService.login(username, password)
-                .then(
-                    user => {
-                        const { from } = this.props.location.state || { from: { pathname: "/" } };
-                        this.props.history.push(from);
-                    },
-                    error => {
-                        setSubmitting(false);
-                        setStatus(error);
-                    }
-                );
-        }}
-        render={({ errors, status, touched, isSubmitting }) => (
-        <Form>
-        <div className="form-group">
-            <label htmlFor="username">Username</label>
-            <Field name="username" type="text" className={'form-control' + (errors.username && touched.username ? ' is-invalid' : '')} />
-        <ErrorMessage name="username" component="div" className="invalid-feedback" />
-            </div>
-            <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <Field name="password" type="password" className={'form-control' + (errors.password && touched.password ? ' is-invalid' : '')} />
-        <ErrorMessage name="password" component="div" className="invalid-feedback" />
-            </div>
-            <div className="form-group">
-            <button type="submit" className="btn btn-primary" disabled={isSubmitting}>Login</button>
-        {isSubmitting &&
-        <img src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
-        }
-    </div>
-        {status &&
-        <div className={'alert alert-danger'}>{status}</div>
-        }
-    </Form>
-    )}
-        />
-        </div>
-    )
+
+    handleChange(event) {
+        let nam = event.target.name;
+        let val = event.target.value;
+        this.setState({[nam]: val});
+    }
+
+    handleSubmit(event) {
+        alert('A username was submitted: ' + this.state.username);
+        alert('A password was submitted: ' + this.state.password);
+        event.preventDefault();
     }
     
-    async populateWeatherData() {
-        const response = await fetch('weatherforecast');
-        const data = await response.json();
-        this.setState({ forecasts: data, loading: false });
+    logInPost()
+    {
+        axios.post('/user', {
+            firstName: this.state.username,
+            lastName: this.state.password
+        })
+            .then(function (response) {
+                console.log(response);
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmit}>
+                <label>
+                    Name:
+                    <input type="text" name='username' value={this.state.username} onChange={this.handleChange}/>
+                </label>
+                <label>
+                    Password:
+                    <input type="password" name='password' value={this.state.password} onChange={this.handleChange}/>
+                </label>
+                <input type="submit" value="Submit"/>
+            </form>
+        );
+    }
 }
-
-
-export { LoginPage }; 
-*/
